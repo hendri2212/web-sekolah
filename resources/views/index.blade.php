a86d60251465c0633850dbb41771d372b426f6e4
rahmadnh <nhrahmad21@gmail.com>
2026-02-03 18:14:13 +0700

    halaman dashboard

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laptop Anda terkena hack</title>
+</head>
+<body>
+    <h1>Silahkan bayar untuk membuka kunci </h1>
+    <h1>NOMOR DANA :085246396463 </h1>
+
+</body>
+</html>
